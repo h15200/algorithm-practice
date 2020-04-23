a5ff8f1939503c01d88f4c372ad51d9150356319
h15200 <45977354+h15200@users.noreply.github.com>
2020-04-22 17:34:23 -0700

    remove from max heap

--- js/maxHeap.js
+++ js/maxHeap.js
@@ -22,6 +22,9 @@ An element's right child: i * 2 + 1
 An element's parent: Math.floor( i / 2 )
 */
 
+// add a remove method as well.
+// to remove, remove root, move last element into the root position, then make sure both children are smaller than root. if not, switch values
+
 var MaxHeap = function () {
   // change code below this line
   this.collection = [];
@@ -53,10 +56,90 @@ var MaxHeap = function () {
   this.print = function () {
     return this.collection;
   };
+
+  /*********************    REMOVE METHOD  ********/
+
+  this.remove = function () {
+    if (this.collection.length < 2) {
+      // doesn't apply if heap doesn't exist. first ele is null
+      return null;
+    }
+    const root = this.collection[1];
+    if (this.collection.length === 2) {
+      // if only root, return a collection with one null
+      this.collection = [null];
+      return root;
+    }
+    if (this.collection.length === 3) {
+      // if only null, root, and one more
+      const temp = this.collection[1];
+      this.collection[1] = this.collection[2];
+      this.collection.pop();
+      return temp;
+    }
+    // heap is at least 4 items long (3 values)
+
+    const lastElement = this.collection[this.collection.length - 1];
+
+    // swap root with last element, effectively removing the root
+    this.collection[1] = lastElement;
+    this.collection.pop(); // remove duplicate last ele
+
+    if (this.collection[1] > this.collection[2] && !this.collection[3]) {
+      // if new root is bigger than only child and there are no more nodes, done
+      return root;
+    } else if (this.collection[3] && this.collection[1] > this.collection[3]) {
+      // if the new root is bigger than both existing children, done
+      return root;
+    }
+
+    // now at least one of the children of the root is bigger
+
+    const checkAndShift = (
+      parentIndex = 1,
+      parent = this.collection[parentIndex],
+      leftChild = this.collection[parentIndex * 2],
+      rightChild = this.collection[parentIndex * 2 + 1]
+    ) => {
+      if (parent < leftChild) {
+        if ((rightChild && leftChild > rightChild) || !rightChild) {
+          const temp = parent;
+          this.collection[parentIndex] = leftChild;
+          this.collection[parentIndex * 2] = temp;
+          checkAndShift(
+            (parentIndex = parentIndex + 1),
+            (parent = this.collection[parentIndex]),
+            (leftChild = this.collection[parentIndex * 2]),
+            (rightChild = this.collection[parentIndex * 2 + 1])
+          );
+        } else if (rightChild && rightChild > leftChild) {
+          const temp = parent;
+          this.collection[parentIndex] = rightChild;
+          this.collection[parentIndex * 2 + 1] = temp;
+          checkAndShift(
+            (parentIndex = parentIndex + 2),
+            (parent = this.collection[parentIndex]),
+            (leftChild = this.collection[parentIndex * 2]),
+            (rightChild = this.collection[parentIndex * 2 + 1])
+          );
+        }
+      }
+    }; // end of checkAndShift()
+
+    checkAndShift();
+    return root;
+  };
+
   // change code above this line
 };
 
 const maxHeap = new MaxHeap();
-maxHeap.insert(1);
-maxHeap.insert(20);
+maxHeap.insert(100);
+// maxHeap.insert(80);
+// maxHeap.insert(70);
+maxHeap.insert(60);
+// maxHeap.insert(5);
+// maxHeap.insert(4);
+// maxHeap.insert(25);
+console.log(maxHeap.remove());
 console.log(maxHeap.print());
